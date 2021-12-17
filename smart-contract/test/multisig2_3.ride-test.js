a83f5f9f3a558d24d3f391d9f5532bdee3fe6093
MrTurtle <support@turtlenetwork.eu>
2021-12-17 14:48:28 +0100

    add helper library, add deployment script

--- smart-contract/test/multisig2_3.ride-test.js
+++ smart-contract/test/multisig2_3.ride-test.js
@@ -1,3 +1,26 @@
-const wvs = 10 ** 8;
+const accountUtils = require("./utils/account.utils");
+const DappUtils = require("./utils/dapp.utils");
 
-describe("multisig2_3 test suite", async function () {});
+let dapp;
+let randomUserAddress;
+
+describe("multisig2_3 test suite", async function () {
+  before(async function () {
+    this.timeout(0);
+
+    await accountUtils.defineAccounts({
+      dapp: 0.1,
+      randomUser: 0.1,
+    });
+
+    await DappUtils.setupDapp("multisig2_3.ride", "dapp");
+    randomUserAddress = address(accounts.randomUser);
+    dapp = address(accounts.dapp);
+  });
+
+  describe("test", () => {
+    it("test", async function () {
+      console.log(randomUserAddress);
+    });
+  });
+});
